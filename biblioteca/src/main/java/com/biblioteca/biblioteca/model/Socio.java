00bfde98b4eacf1e2b260abb0c9c8d1815bbf7f9
NahuelDesimone <nahuel.desimone98@gmail.com>
2021-06-27 08:36:23 -0300

    Se agrega la tabla de socios

---
+++ biblioteca/src/main/java/com/biblioteca/biblioteca/model/Socio.java
@@ -0,0 +1,61 @@
+package com.biblioteca.biblioteca.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name = "SOCIO")
+
+public class Socio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "ID_SOCIO")
+    Long idSocio;
+
+    @Column(name = "NOMBRE")
+    String nombre;
+
+    @Column(name = "APELLIDO")
+    String apellido;
+
+    @Column(name = "EMAIL")
+    String email;
+
+    public Long getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(Long idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
